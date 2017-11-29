48b77dbeae16dc30efea4948ead63c21207ad49d
mcbalsam <33693337+mcbalsam@users.noreply.github.com>
2017-11-29 17:59:11 +0200

    Lisätty tiedostot ja päivitetty Bootstrap 4:een

---
+++ HTML/js/script.js
@@ -0,0 +1,30 @@
+
+
+// Tähän alle laskettu aika ja syntymäaika
+
+
+var laskettuAika = new Date("2017-03-21");
+var syntymaAika = new Date("2017-03-28");
+
+var vanhempainvapaa = {
+  paivat: 158
+};
+
+var aitiysvapaa = {
+  paivat: 105
+};
+
+var isyysvapaa = {
+  paivat: 54
+};
+
+
+/*
+Tähän kohtaan erilaisia testikomentoja
+
+console.log(vanhempainvapaa.paivat);
+console.log(laskettuAika);
+console.log(laskettuAika.setDate(laskettuAika.getDate()+50));
+*/
+
+getDate();
